c3e76a78c93eb786ae2c88d4db0db026ced05277
artiom_svistunov <ArtemSvist1809@gmail.com>
2023-07-30 15:48:25 +0300

    AirportService 2.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,49 +8,73 @@ import java.util.Scanner;
 public class Main {
 
     private static final AirportService airportService = new AirportService(new ArrayList<>());
-
     private static final List<String> airportCodes = new JSONParser().parseAirportCodeJson("resources/airports.json");
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int command = 10;
-        Scanner scanner = new Scanner(System.in);
         System.out.println(ConsoleMessages.AIRPORT_SERVICE);
+
         while (command != 0){
             System.out.print(ConsoleMessages.MAIN_MENU + ConsoleMessages.MENU_ITEM_NUMBER);
             command = scanner.nextInt();
             scanner.nextLine();
-            if(command == 1){
-                System.out.println(ConsoleMessages.FLIGHT_DATA);
-                System.out.print(ConsoleMessages.FLIGHT_NUMBER);
-                String flightNumber = scanner.nextLine();
-                System.out.print(ConsoleMessages.DATE);
-                String date = scanner.nextLine();
-                System.out.print(ConsoleMessages.DEPARTURE_TIME);
-                String departureTime = scanner.nextLine();
-                System.out.print(ConsoleMessages.FLIGHT_TIME);
-                String flightTime = scanner.nextLine();
-                System.out.print(ConsoleMessages.CODE_IATA_DEPARTURE);
-                String codeIATADeparture = scanner.nextLine();
-                System.out.print(ConsoleMessages.CODE_IATA_ARRIVAL);
-                String codeIATAArrival = scanner.nextLine();
-                System.out.print(ConsoleMessages.PRICE);
-                String price = scanner.nextLine();
-                if (isFlightCorrect(flightNumber, date, departureTime, flightTime, codeIATADeparture, codeIATAArrival, price)){
-                    airportService.addFlight(flightNumber.toUpperCase(), LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
-                            LocalTime.parse(departureTime, DateTimeFormatter.ofPattern("HH:mm")),
-                            flightTime, codeIATADeparture, codeIATAArrival, Double.valueOf(price));
-                    System.out.println(airportService.getFlight(flightNumber.toUpperCase()).replace("\n", "") + " добавлена\n");
-                } else{
-                    System.out.println(ConsoleMessages.ERROR_DATA_FLIGHT_FORMAT);
-                }
-            } else if (command == 2) {
-                System.out.println(airportService.getAllFlight());
-            } else if (command == 3) {
-                System.out.print(ConsoleMessages.ENTER_FLIGHT_NUMBER);
-                String flightNumber = scanner.nextLine();
-                System.out.println(airportService.getFlight(flightNumber.toUpperCase()));
+
+            switch (command) {
+                case 1 -> addFlight();
+                case 2 -> System.out.println(airportService.getAllFlight());
+                case 3 -> getFlightByNumber();
             }
         }
+    }
+
+    private static void addFlight() {
+        System.out.println(ConsoleMessages.FLIGHT_DATA);
+
+        String flightNumber = readStringWithFormat(ConsoleMessages.FLIGHT_NUMBER, "^[A-Za-z]{4}$");
+        String date = readStringWithFormat(ConsoleMessages.DATE, ".{10}");
+        String departureTime = readStringWithFormat(ConsoleMessages.DEPARTURE_TIME, ".{5}");
+        String flightTime = readStringWithFormat(ConsoleMessages.FLIGHT_TIME, "^\\d{2}\\.\\d{2}$");
+        String codeIATADeparture = readStringWithFormat(ConsoleMessages.CODE_IATA_DEPARTURE, ".{3}");
+        String codeIATAArrival = readStringWithFormat(ConsoleMessages.CODE_IATA_ARRIVAL, ".{3}");
+        String price = readStringWithFormat(ConsoleMessages.PRICE, "^\\d+(\\.\\d+)?$");
+
+        if (isFlightCorrect(flightNumber, date, departureTime, flightTime, codeIATADeparture, codeIATAArrival, price)) {
+            airportService.addFlight(
+                    flightNumber.toUpperCase(),
+                    LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                    LocalTime.parse(departureTime, DateTimeFormatter.ofPattern("HH:mm")),
+                    flightTime, codeIATADeparture, codeIATAArrival, Double.valueOf(price)
+            );
+            System.out.println(airportService.getFlight(flightNumber.toUpperCase()).replace("\n", "") + " добавлена\n");
+        } else {
+            System.out.println(ConsoleMessages.ERROR_DATA_FLIGHT_FORMAT);
+        }
+    }
+
+    private static void getFlightByNumber() {
+        System.out.print(ConsoleMessages.ENTER_FLIGHT_NUMBER);
+        String flightNumber = scanner.nextLine();
+        System.out.println(airportService.getFlight(flightNumber.toUpperCase()));
+    }
+
+    private static String readStringWithFormat(String message, String regexPattern) {
+        String input;
+        boolean isValid;
+        do {
+            System.out.print(message);
+            input = scanner.nextLine();
+            isValid = input.matches(regexPattern);
+
+            if (isValid && (message.equals(ConsoleMessages.CODE_IATA_DEPARTURE) || message.equals(ConsoleMessages.CODE_IATA_ARRIVAL))) {
+                isValid = airportCodes.contains(input);
+            }
+
+            if (!isValid) {
+                System.out.println("Недопустимый формат данных. Пожалуйста, повторите ввод.");
+            }
+        } while (!isValid);
+        return input;
     }
 
     private static boolean isFlightCorrect(String flightNumber, String date, String departureTime, String flightTime,
